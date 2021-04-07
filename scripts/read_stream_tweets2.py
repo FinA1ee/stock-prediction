ec69fe59f6381f1def09675de903976d749a1d19
y242su <y242su@student.cs.uwaterloo.ca>
2021-04-07 12:08:17 -0400

    add predictors and csv

--- scripts/read_stream_tweets2.py
+++ scripts/read_stream_tweets2.py
@@ -58,11 +58,15 @@ if __name__ == '__main__':
             continue
 
     tweet_dict = {}
+    # tweet_dict[''] = []
     tweet_dict['text'] = []
     tweet_dict['retweet_count'] = []
     tweet_dict['favorite_count'] = []
     tweet_dict['author_followers_count'] = []
+    tweet_dict['author_listed_count'] = []
+    tweet_dict['author_statuses_count'] = []
     tweet_dict['author_friends_count'] = []
+    tweet_dict['author_favourites_count'] = []
     tweet_dict['hashtag_indices'] = []
 
     # extract info from raw data
@@ -70,11 +74,15 @@ if __name__ == '__main__':
     for id in tweet_ids:
         try:
             status = api.get_status(id)
+            # tweet_dict[''].append()
             tweet_dict['text'].append(status.text)
             tweet_dict['retweet_count'].append(status.retweet_count)
             tweet_dict['favorite_count'].append(status.favorite_count)
             tweet_dict['author_followers_count'].append(status.author.followers_count)
+            tweet_dict['author_listed_count'].append(status.author.listed_count)
+            tweet_dict['author_statuses_count'].append(status.author.statuses_count)
             tweet_dict['author_friends_count'].append(status.author.friends_count)
+            tweet_dict['author_favourites_count'].append(status.author.favourites_count)
             tweet_hashtags = []
             for hashtag in status.entities['hashtags']:
                 tweet_hashtags.append(hashtag['indices'])
@@ -84,13 +92,47 @@ if __name__ == '__main__':
             continue
 
     # print info
-    i = 0
-    while i < size:
-        likes = tweet_dict['favorite_count'][i]
-        retweets = tweet_dict['retweet_count'][i]
-        text = tweet_dict['text'][i].encode('utf-8').replace("\n", ". ") # remove line breakers
-        followers = tweet_dict['author_followers_count'][i]
-        friends = tweet_dict['author_friends_count'][i]
-        hashtags = tweet_dict['hashtag_indices'][i]
-        print(likes, retweets, text, followers, friends, hashtags)
-        i += 1
+    if 1:
+        i = 0
+        while i < size:
+            # = tweet_dict[''][i]
+            retweets = tweet_dict['retweet_count'][i]
+            likes = tweet_dict['favorite_count'][i]
+            author_followers = tweet_dict['author_followers_count'][i]
+            author_listed = tweet_dict['author_listed_count'][i]
+            author_statuses = tweet_dict['author_statuses_count'][i]
+            author_friends = tweet_dict['author_friends_count'][i]
+            author_favourites = tweet_dict['author_favourites_count'][i]
+            hashtags = tweet_dict['hashtag_indices'][i]
+            text = tweet_dict['text'][i].encode('utf-8').replace("\n", ". ") # remove line breakers
+            # add to output
+            print(retweets, likes, author_followers, author_listed, author_statuses, author_friends, \
+                author_favourites, hashtags, text)
+            i += 1
+
+    # output to csv
+    # else:
+        tweets = []
+        i = 0
+        while i < size:
+            tweet = []
+            tweet.append(i)
+            # tweet.append(tweet_dict[''][i])
+            tweet.append(tweet_dict['retweet_count'][i])
+            tweet.append(tweet_dict['favorite_count'][i])
+            tweet.append(tweet_dict['author_followers_count'][i])
+            tweet.append(tweet_dict['author_listed_count'][i])
+            tweet.append(tweet_dict['author_statuses_count'][i])
+            tweet.append(tweet_dict['author_friends_count'][i])
+            tweet.append(tweet_dict['author_favourites_count'][i])
+            tweet.append(tweet_dict['hashtag_indices'][i])
+            tweet.append(tweet_dict['text'][i].encode('utf-8').replace("\n", ". ")) # remove line breakers
+            tweets.append(tweet)
+            i += 1
+
+        # add colname to dataframe
+        df = pd.DataFrame(tweets, columns=['id', 'retweet_count', 'favorite_count', 'author_followers_count', \
+            'author_listed_count', 'author_statuses_count', 'author_friends_count', \
+            'author_favourites_count', 'hashtag_indices', 'text'])
+        df.to_csv(output_path + ".csv", index=False)
+
