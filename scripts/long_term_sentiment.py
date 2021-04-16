067eae49dd2f33026892822a166a20b90bdf23a1
Shuxin Ye <s53ye@uwaterloo.ca>
2021-04-16 06:21:14 -0400

    merge all 3 years

--- scripts/long_term_sentiment.py
+++ scripts/long_term_sentiment.py
@@ -11,7 +11,7 @@ import pyspark.sql.functions as F
 import pandas as pd
 import time
 from pyspark.sql.window import Window
-from pyspark.sql.types import DoubleType, ArrayType
+from pyspark.sql.types import DoubleType, ArrayType, IntegerType
 from textblob import TextBlob
 
 spark = SparkSession \
@@ -33,13 +33,16 @@ tweets = spark.read.csv("data/long_term_processed_"+target_year+"/*.csv", header
 tweets = tweets.withColumn("sentiment", udf_get_sentiment("body"))
 tweets = tweets.withColumn("polarity", tweets.sentiment.getItem(0)) \
 	.withColumn("subjectivity", tweets.sentiment.getItem(1)) \
-	.drop("sentiment") \
+	.drop("sentiment", "body", "tweet_id", "writer", "post_date", "post_month")
+
+tweets = tweets.withColumn("Trend",tweets.Trend.cast(IntegerType()))
+tweets = tweets.withColumn("comment_num",tweets.comment_num.cast(IntegerType()))
+tweets = tweets.withColumn("retweet_num",tweets.comment_num.cast(IntegerType()))
+tweets = tweets.withColumn("like_num",tweets.comment_num.cast(IntegerType()))	\
 	.write.format('csv') \
     .option('header',True) \
     .mode('overwrite') \
     .option('sep',',') \
     .save('data/long_term_sentiment_{}'.format(target_year))
-
-
 
 
